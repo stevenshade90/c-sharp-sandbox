bcfca217e5c393c185dadf4f595d2791e87ad547
Steven Shade <stevenshade90@gmail.com>
2026-09-02 16:41:38 -0400

    Add SoundPlayer_Practice and LinkedList_Practice modules

---
+++ LinkedList_Practice/LinkedList_Practice/Program.cs
@@ -0,0 +1,104 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Text;
+
+namespace LinkedList_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] words = { "the", "dog", "jumps", "over", "the", "log" };
+            LinkedList<string> sentence = new LinkedList<string>(words);
+
+            DisplayAllWords(sentence, "Original LinkedList values:");
+            GetNode(sentence);
+
+            AddNodes(sentence, new string[] { "and", "the", "hog" });
+            DisplayAllWords(sentence, "After adding new nodes to the end of the LinkedList:");
+
+            sentence.AddBefore(sentence.Find("dog"), "big");
+            DisplayAllWords(sentence, "After adding a node before 'dog':");
+
+            LinkedListNode<string> dogNode = sentence.Find("dog");
+            Console.WriteLine($"Value before 'dog' node: {dogNode.Previous.Value}");
+            Console.WriteLine($"Value after 'dog' node: {dogNode.Next.Value}");
+            
+            sentence.AddBefore(dogNode, "ugly");
+            sentence.AddAfter(dogNode, "barely");
+            Console.WriteLine($"Value after adding 'ugly' before 'dog': {dogNode.Previous.Value}");
+            Console.WriteLine($"Value after adding 'barely' after 'dog': {dogNode.Next.Value}\n");
+
+            DisplayAllWords(sentence, "After adding nodes before and after 'dog':");
+
+
+            LinkedList<string> linkedList2 = new LinkedList<string>(new string[] { "the", "big", "dog" });
+            Console.WriteLine("Working on new LinkedList values");
+            ReverseSentence(linkedList2, "Reversed LinkedList values:");
+
+            FindAndReplace(linkedList2, "\nFinding and replacing 'dog' with 'cat'");
+        }
+
+        static void DisplayAllWords(LinkedList<string> mySentence, string text)
+        {
+            Console.WriteLine(text);
+            foreach (string word in mySentence)
+            {
+                Console.Write(word + " ");
+            }
+            Console.WriteLine("\n");
+        }
+
+        static void GetNode(LinkedList<string> mySentence)
+        {
+            LinkedListNode<string> firstNode = mySentence.First;
+            LinkedListNode<string> lastNode = mySentence.Last;
+
+            Console.WriteLine("First node value: " + firstNode.Value);
+            Console.WriteLine("Last node value: " + lastNode.Value);
+
+            Console.WriteLine("\n");
+        }
+
+        static void AddNodes(LinkedList<string> mySentence, params string[] words)
+        {
+            foreach (string word in words)
+            {
+                mySentence.AddLast(word);
+            }
+        }
+
+        static void ReverseSentence(LinkedList<string> mySentence, string text)
+        {
+            LinkedListNode<string> currentNode = mySentence.Last;
+            Console.WriteLine(text + " ");
+
+            while (currentNode != null)
+            {
+                Console.Write(currentNode.Value + " ");
+                currentNode = currentNode.Previous;
+            }
+            Console.WriteLine();
+        }
+
+        static void FindAndReplace(LinkedList<string> mySentence, string text)
+        {
+            Console.WriteLine(text);
+
+            LinkedListNode<string> findNode = mySentence.Find("dog");
+
+            if (findNode != null)
+            {
+                string currentNodeValue = findNode.Value;
+                findNode.Value = "cat";
+
+                Console.WriteLine($"Replaced '{currentNodeValue}' with '{findNode.Value}'");
+                DisplayAllWords(mySentence, "\nUpdated LinkedList values:");
+            }
+            else
+            {
+                Console.WriteLine($"Could not find the word in the LinkedList.");
+            }
+        }
+    }
+}
---
+++ SoundPlayer_Practice/SoundPlayer_Practice/Program.cs
@@ -0,0 +1,61 @@
+﻿using System.Media;
+
+namespace SoundPlayer_Practice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            List<SoundPlayer> collectionOfMusic = new List<SoundPlayer>()
+            {
+                new SoundPlayer
+                {
+                    SoundLocation = @"C:\Users\steve\OneDrive\Desktop\Dorico Files\I. Catalog of Original Works\Orchestral Overture (PDF, MP3)\Orchestral Overture - Audio.wav",
+                    Tag = "Orchestral Overture"
+                },
+                new SoundPlayer
+                {
+                    SoundLocation = @"C:\Users\steve\OneDrive\Desktop\Flows from Inferno\01 - Inferno\Inferno_Dorico - Inferno.wav",
+                    Tag = "Inferno"
+                },
+                new SoundPlayer
+                {
+                    SoundLocation = @"C:\Users\steve\OneDrive\Desktop\Reaper Projects\Schubert\Impromptus, Op_ 90\No_ 3 in Gb Major\gb.wav",
+                    Tag = "Impromptu No. 3"
+                }
+            };
+
+            DisplaySongs(collectionOfMusic);
+            GetSong(collectionOfMusic);
+        }
+
+        static void DisplaySongs(List<SoundPlayer> collection)
+        {
+            for (int i = 0; i < collection.Count; i++)
+            {
+                Console.WriteLine($"{i+1}: {collection[i].Tag}");
+            }
+        }
+
+        static void GetSong(List<SoundPlayer> collection)
+        {
+            Console.Write("Select a song to play:");
+
+            int choice = int.TryParse(Console.ReadLine(), out int y) ? y - 1 : 0;
+
+            SoundPlayer player = collection[choice];
+            Console.WriteLine(player.Tag);
+
+            try
+            {
+                player.Play();
+            }
+            catch (FileNotFoundException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
